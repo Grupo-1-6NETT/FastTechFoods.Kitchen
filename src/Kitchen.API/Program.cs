dca69f1e46d9e011a4791ef9e8335fb25662523d
b01tech <bruno01.tech@gmail.com>
2025-07-16 11:36:49 -0300

    feat: adiciona configuracao do prometheus

--- src/Kitchen.API/Program.cs
+++ src/Kitchen.API/Program.cs
@@ -3,6 +3,7 @@ using Kitchen.Infrastructure.DependencyInjection;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Prometheus;
 using System.Reflection;
 using System.Text;
 
@@ -82,12 +83,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+
 app.UseHttpsRedirection();
+
+app.UseHttpMetrics();
 
 app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapMetrics();
 
 app.ApplyMigrations();
 
