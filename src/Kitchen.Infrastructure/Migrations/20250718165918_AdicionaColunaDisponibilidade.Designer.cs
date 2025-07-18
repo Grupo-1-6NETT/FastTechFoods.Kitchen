fe74c6e7875754ddcb7c5e214b7f48523ebcc81e
b01tech <bruno01.tech@gmail.com>
2025-07-18 14:00:13 -0300

    feat: adiciona migration adiciona coluna forma de pagamento

--- src/Kitchen.Infrastructure/Migrations/20250718165918_AdicionaColunaDisponibilidade.Designer.cs
+++ src/Kitchen.Infrastructure/Migrations/20250718165918_AdicionaColunaDisponibilidade.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Kitchen.Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -11,9 +12,11 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace Kitchen.Infrastructure.Migrations
 {
     [DbContext(typeof(KitchenDbContext))]
-    partial class KitchenDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250718165918_AdicionaColunaDisponibilidade")]
+    partial class AdicionaColunaDisponibilidade
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Kitchen.Infrastructure/Migrations/20250718165918_AdicionaColunaDisponibilidade.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Kitchen.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AdicionaColunaDisponibilidade : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "FormaDeEntrega",
+                table: "Pedidos",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Justificativa",
+                table: "Pedidos",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FormaDeEntrega",
+                table: "Pedidos");
+
+            migrationBuilder.DropColumn(
+                name: "Justificativa",
+                table: "Pedidos");
+        }
+    }
+}
